aabc139d1fa655a3ccf7e98d597536814ff47bb3
renamethis <112321Johnny>
2019-03-17 17:25:44 +0300

    Update

--- bottelega.py
+++ bottelega.py
@@ -61,7 +61,7 @@ class BotHandler:
                         #print(records)
                         with conn.cursor() as cursor:
                             conn.autocommit = True
-                            insert = sql.SQL('INSERT INTO users (user_id, subs) VALUES {}').format(sql.SQL(',').join(map(sql.Literal, (str(chat_id), 'all'))))
+                            insert = sql.SQL('INSERT INTO users (user_id, subs) VALUES {}').format(sql.SQL(',').join(map(sql.Literal, (chat_id, 'all'))))
                             cursor.execute(insert)
                         fci.write(str(chat_id) + '\n')
                         self.send_mess(chat_id, "Вы подписались на отправку новостей!")
